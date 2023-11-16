64c5a5815e285219857138e896589c92bcf5c3c0
sonnt <ngotheson1@gmail.com>
2023-11-16 12:22:05 +0700

    fix: data country

--- src/pageComponent/Destination/SectionActions.js
+++ src/pageComponent/Destination/SectionActions.js
@@ -1,9 +1,20 @@
 import fetchData from '@/data/fetchData'
+import { DATA_COUNTRY, DATA_ICONS_COUNTRY } from '@/graphql/country/queries'
 import ListAction from './ListAction'
-import { DATA_ICONS_COUNTRY } from '@/graphql/country/queries'
-async function SectionActions({ infoCountry, lang }) {
-  const dataIcons = await fetchData(DATA_ICONS_COUNTRY, { language: lang.toUpperCase(), slug: "cG9zdDozMDQ1" })
+async function SectionActions({ lang, slug }) {
+  const [dataCountry, dataIcons] = await Promise.all([
+    fetchData(DATA_COUNTRY, {
+      language: lang?.toUpperCase(),
+      taxonomyValue: slug
+    }),
+    fetchData(DATA_ICONS_COUNTRY, { language: lang.toUpperCase(), slug: "cG9zdDozMDQ1" })
+  ])
+
   const listActions = dataIcons?.data?.page?.translation
+
+  const data = dataCountry?.data?.countries
+
+  const infoCountry = data.translation.country?.info
 
   const arrInfoCountry = []
   arrInfoCountry.push(infoCountry?.population)
@@ -12,6 +23,8 @@ async function SectionActions({ infoCountry, lang }) {
   arrInfoCountry.push(infoCountry?.currency)
   arrInfoCountry.push(infoCountry?.wheather)
   arrInfoCountry.push(infoCountry?.timze)
+
+
   return (
     <div className='md:px-[10.25vw] justify-between px-[8vw] md:rounded-[0] rounded-[4.27vw] md:mt-[4vw] relative md:top-0 top-[-5vw] bg-white md:pt-[0] pt-[16.53vw] md:pb-[0] pb-[15vw] grid max-md:grid-cols-3 max-md:gap-y-[6.53vw] max-md:gap-x-[14.93vw] grid-cols-6 gap-[7.38vw] md:grid-rows-1'>
       {listActions?.travelStyle?.icons?.map((action, index) => (
--- src/pageComponent/Destination/index.js
+++ src/pageComponent/Destination/index.js
@@ -1,8 +1,3 @@
-import NotFound from '@/components/Common/NotFound'
-import fetchData from '@/data/fetchData'
-import {
-  DATA_COUNTRY
-} from '@/graphql/country/queries'
 import Banner from './Banner'
 import CustomerReview from './CustomerReview'
 import FilterPopup from './FilterPopup'
@@ -10,14 +5,6 @@ import OurBlog from './OurBlog'
 import SectionActions from './SectionActions'
 import SlideDestination from './SlideDestination'
 async function index({ lang, slug }) {
-  const dataCountry = await fetchData(DATA_COUNTRY, {
-    language: lang?.toUpperCase(),
-    taxonomyValue: slug
-  })
-  const data = dataCountry?.data?.countries?.translation
-  if (!data) {
-    return <NotFound lang={lang} />
-  }
   return (
     <div>
       <Banner
@@ -25,7 +12,7 @@ async function index({ lang, slug }) {
         lang={lang}
       />
       <FilterPopup />
-      <SectionActions lang={lang} infoCountry={data?.country?.info} />
+      <SectionActions lang={lang} slug={slug} />
       <SlideDestination
         lang={lang}
         slug={slug}
