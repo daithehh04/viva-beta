fcd976f6992940d29fa013fa5aba45533fd97115
sonnt <ngotheson1@gmail.com>
2023-12-21 17:31:46 +0700

    fix: build type of trip

--- src/pageComponent/TravelStyle/index.js
+++ src/pageComponent/TravelStyle/index.js
@@ -11,11 +11,12 @@ import HotTour from './HotTour'
 import TourSlide from './TourSlide'
 import fetchData from '@/data/fetchData'
 import { getDictionary } from '@/get-dictionary'
+import { Suspense } from 'react'
 
 async function index({ lang, slug }) {
   const getPageInfo = await fetchData(GET_INFO_PAGE_TRAVEL_STYLE, {
     language: lang?.toUpperCase(),
-    taxonomyValue: slug,
+    taxonomyValue: slug
   })
 
   if (!getPageInfo?.data?.tourStyle?.translation?.banner) {
@@ -29,22 +30,29 @@ async function index({ lang, slug }) {
   const dataTaxonomiesDuration = await fetchData(DATA_TAXONOMIES_DURATION, { language: lang?.toUpperCase() })
   return (
     <div>
-      <Banner slug={slug} lang={lang}/>
-      <TourSlide
-        tourStyleName={getPageInfo?.data?.tourStyle?.translation?.banner?.tourstylename}
-        lang={lang}
-        slug={getPageInfo?.data?.tourStyle?.translation?.slug}
-        dataTaxonomiesCountry={dataTaxonomiesCountry}
-        dataTaxonomiesStyleTour={dataTaxonomiesStyleTour}
-        dataTaxonomiesBudget={dataTaxonomiesBudget}
-        dataTaxonomiesDuration={dataTaxonomiesDuration}
-        dictionary={dictionary}
-      />
-      <HotTour
+      <Banner
         slug={slug}
         lang={lang}
-        dictionary={dictionary}
       />
+      <Suspense fallback={<div>Loading...</div>}>
+        <TourSlide
+          tourStyleName={getPageInfo?.data?.tourStyle?.translation?.banner?.tourstylename}
+          lang={lang}
+          slug={getPageInfo?.data?.tourStyle?.translation?.slug}
+          dataTaxonomiesCountry={dataTaxonomiesCountry}
+          dataTaxonomiesStyleTour={dataTaxonomiesStyleTour}
+          dataTaxonomiesBudget={dataTaxonomiesBudget}
+          dataTaxonomiesDuration={dataTaxonomiesDuration}
+          dictionary={dictionary}
+        />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <HotTour
+          slug={slug}
+          lang={lang}
+          dictionary={dictionary}
+        />
+      </Suspense>
     </div>
   )
 }
